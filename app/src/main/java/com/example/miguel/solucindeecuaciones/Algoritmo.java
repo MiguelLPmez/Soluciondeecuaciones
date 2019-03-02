8702b687c15b1a6b82084eb32303cb49709401d9
Joan Garcia <joangarcia2015@gmail.com>
2019-03-02 14:51:26 -0600

    Creación de clase Algoritmo.

---
+++ app/src/main/java/com/example/miguel/solucindeecuaciones/Algoritmo.java
@@ -0,0 +1,11 @@
+package com.example.miguel.solucindeecuaciones;
+
+public class Algoritmo {
+    Ecuación ec;
+
+    public Algoritmo(int a, int b, int c, int d, int e){
+        ec = new Ecuación(a, b, c, d, e);
+    }
+
+    // - Crear aqui los metodos bisección, newton, secante y regla falsa.
+}
