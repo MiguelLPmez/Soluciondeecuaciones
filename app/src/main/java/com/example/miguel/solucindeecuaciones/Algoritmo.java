9eb306e397c57856edefcb32b68604f12a57c2af
MiguelLPmez <mig.angel.1916@gmail.com>
2019-03-07 21:40:26 -0600

    Se agrega método de Newton

--- app/src/main/java/com/example/miguel/solucindeecuaciones/Algoritmo.java
+++ app/src/main/java/com/example/miguel/solucindeecuaciones/Algoritmo.java
@@ -1,11 +1,55 @@
 package com.example.miguel.solucindeecuaciones;
 
 public class Algoritmo {
-    Ecuación ec;
+    private final Ecuación ec;
+    private double solución, errorF;
 
-    public Algoritmo(int a, int b, int c, int d, int e){
-        ec = new Ecuación(a, b, c, d, e);
+    public Algoritmo(Ecuación ec){
+        this.ec = ec;
     }
+
+    // - Retorna el error final de la última llamada al método que da la solución por un determinado
+    //   algoritmo.
+    public double getErrorFinal(){
+        return errorF;
+    }
+
+    // - Aplica el algoritmo de Newton-Raphson y regresa la solución.
+    public double getSoluciónNewton(double p0, double tol, int itmax){
+        algoritmoNewton(p0, tol, itmax);
+        return solución;
+    }
+
+    // - Aplica el algoritmo de la secante y regresa la solución.
+    public double getSoluciónSecante(double p0, double p1, double tol, int itmax){
+        algoritmoSecante(p0, p1, tol, itmax);
+        return solución;
+    }
+
+
+    /*              - Implementación de los algorimos -             */
+
+    private void algoritmoNewton(double p0, double tol, int itmax){
+        for (int i = 0; i < itmax; i++) {
+            solución = p0 - ec.evaluarEn(p0) / ec.derivadaEn(p0);
+            errorF = Math.abs(solución - p0);
+            if (errorF <= tol) break;
+            p0 = solución;
+        }
+
+    }
+
+    private void algoritmoSecante(double p0, double p1, double tol, int itmax){
+        for (int i = 0; i < itmax; i++) {
+            solución = p1 - (ec.evaluarEn(p1) * (p1 - p0)) / (ec.evaluarEn(p1) - ec.evaluarEn(p0));
+            errorF = Math.abs(solución - p1);
+            if (errorF <= tol) break;
+            p0 = p1;
+            p1 = solución;
+        }
+    }
+
+    /*          De versión anterior de la clase:
 
     // - Crear aqui los metodos bisección, newton, secante y regla falsa.
 
@@ -13,22 +57,24 @@ public class Algoritmo {
         int i = 0;
         double fp0 = ec.evaluarEn(p0);
         double fp1 = ec.evaluarEn(p1);
-        double p, fp;
+        double fp, p;
 
-        while (i < itmax){
+        while (i <= itmax){
             p = p1 - fp1 * (p1 - p0) / (fp1 - fp0);
             fp = ec.evaluarEn(p);
-            if (Math.abs(fp) < tol){
+            if (Math.abs(fp - p) < tol)
                 return p;
-            }
             i++;
             p0 = p1;
             p1 = p;
         }
 
-        /*if (Math.abs(fp) > tol){
-            return -1;
-        }*/
+        //if (Math.abs(fp) > tol){
+        //    return -1;
+        //}
+
         return -1;
     }
+
+    */
 }
